c42a75741aa2e6ab75d38cbeb92d41d6c69d550a
Santyls <123048209@upq.edu.mx>
2025-10-15 14:44:50 -0600

    Controlador
    Se creo el controlador CalculadoraController

--- GitFlow_ejemp/routes/web.php
+++ GitFlow_ejemp/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/division/{num1}/{num2}', [CalculadoraController::class, 'division']);
