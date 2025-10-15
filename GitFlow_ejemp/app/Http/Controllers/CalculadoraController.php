05a96c6e14b27da7c9d841244e33291c1f29526f
Santyls <123048209@upq.edu.mx>
2025-10-15 15:23:23 -0600

    Vista de calculadora y funcion de division
    Se creo la vista calculadora y se añadio  la funcion de division

--- GitFlow_ejemp/app/Http/Controllers/CalculadoraController.php
+++ GitFlow_ejemp/app/Http/Controllers/CalculadoraController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CalculadoraController extends Controller
 {
-    //
+    public function suma($opcion,$num1,$num2){
+        return view('calculadora.calculadora',['opcion' => $opcion ,'num1' => $num1, 'num2' => $num2]);
+    }
+
 }
--- GitFlow_ejemp/routes/web.php
+++ GitFlow_ejemp/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalculadoraController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/division/{num1}/{num2}', [CalculadoraController::class, 'division']);
+Route::get('calculadora/{opcion}/{num1}/{num2}', [CalculadoraController::class, 'suma']);
