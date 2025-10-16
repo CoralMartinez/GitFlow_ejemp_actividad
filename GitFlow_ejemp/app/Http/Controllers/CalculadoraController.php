d19585a5c06cb1fcb8ee2e84ee5865cbcbdd1dfb
taniarmhm <mejiatt56@gmail.com>
2025-10-15 19:56:35 -0600

    suma1

--- GitFlow_ejemp/app/Http/Controllers/CalculadoraController.php
+++ GitFlow_ejemp/app/Http/Controllers/CalculadoraController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class CalculadoraController extends Controller
 {
    public function suma($num1, $num2){
-        return 'la suma de '.$num1.' y '.$num2.' es: '.($num1 + $num2);
+        return 'la sumaaaaa de '.$num1.' y '.$num2.' es: '.($num1 + $num2);
     }
 }
 
