a4c18fb715a4cdf714168ca8b169d485974acc89
taniarmhm <mejiatt56@gmail.com>
2025-10-15 19:49:58 -0600

    suma

--- GitFlow_ejemp/app/Http/Controllers/CalculadoraController.php
+++ GitFlow_ejemp/app/Http/Controllers/CalculadoraController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class CalculadoraController extends Controller
 {
-    //
+   public function suma($num1, $num2){
+        return 'la suma de '.$num1.' y '.$num2.' es: '.($num1 + $num2);
+    }
 }
+
+
--- GitFlow_ejemp/routes/web.php
+++ GitFlow_ejemp/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalculadoraController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/division/{num1}/{num2}', [CalculadoraController::class, 'division']);
+
+Route::get('/suma/{num1}/{num2}', [CalculadoraController::class, 'suma']);
+
+
